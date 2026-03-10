13e1b78d8980f6322a93a36525ee0d4c0fcfbbc3
Kien Nguyen <kinnguynhu@gmail.com>
2026-03-10 15:18:42 +0000

    Add script for prototype pollution database creation and query execution

---
+++ scripts/test.prototype.pollution.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p results
+
+pushd prototype-pollution-2.0
+
+# Create db
+if [ -d "/app/results/prototype-pollution-2.0" ]; then
+    echo "Skip"
+else
+    echo "Create database"
+    /app/codeqls/codeql-2.24.0/codeql database create "/app/results/prototype-pollution-2.0" --language=javascript --overwrite
+    /app/codeqls/codeql-2.24.0/codeql database finalize "/app/results/prototype-pollution-2.0"
+fi
+# Run query
+echo "Run query"
+
+/app/codeqls/codeql-2.24.0/codeql database analyze \
+  /app/results/prototype-pollution-2.0 \
+  --format=csv \
+  --output=/app/results/prototype-pollution-2.0.csv \
+  --threads=2 
+
+popd
